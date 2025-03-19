25dcc44ae328a7dbee7d233d94418b6f228a21a9
mrutyunjaya07 <mjmohanty3058@gmail.com>
2025-03-19 20:19:54 +0530

    Update 1.create_a_array_by_taking_user_input.py

--- NUMPY/1.create_a_array_by_taking_user_input.py
+++ NUMPY/1.create_a_array_by_taking_user_input.py
@@ -1,9 +1,13 @@
+# import numpy liabrary
 import numpy as np
 
+# initialize a empty list
 l=[]
 
+# taking input
 for i in range(5):
     int_1=int(input("Enter a integer :"))
     l.append(int_1)
 
+# creating numpy array by passing the list
 print(np.array(l))
